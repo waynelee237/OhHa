85e4b6e083a49198abb064a360409f163787e7fd
Wayne <wayneleez@hotmail.com>
2013-05-13 17:55:51 +0300

    Ohjelmoinnin harjoitustyö

---
+++ OhHaT/src/ohhat/OhHaT.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohhat;
+
+/**
+ *
+ * @author wayne
+ */
+public class OhHaT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
